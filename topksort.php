5a1d88cde2115e19a56e0013e9faa13a04dc0171
Dany Thach <dany.t@complic.eu>
2021-08-03 13:42:04 +0200

    Avoiding declaration of helper functions

--- topksort.php
+++ topksort.php
@@ -1,54 +1,52 @@
 <?php
 
-function swap(&$array, $i, $j) {
-  $tmp = $array[$i];
-  $array[$i] = $array[$j];
-  $array[$j] = $tmp;
-}
-
-function topkheapifydown(&$array, $k) {
-  $i = 0;
-  do {
-    $done = true;
-    $maxIdx = $i;
-    if(2*($i+1)-1 < $k) {
-      if($array[2*($i+1)-1] > $array[$maxIdx]) {
-        $maxIdx = 2*($i+1)-1;
-      }
-      if(2*($i+1) < $k) {
-        if($array[2*($i+1)] > $array[$maxIdx]) {
-          $maxIdx = 2*($i+1);
+function topksort(&$array, $k = 0) {
+  $swap = function (&$array, $i, $j) {
+    $tmp = $array[$i];
+    $array[$i] = $array[$j];
+    $array[$j] = $tmp;
+  };
+  $topkheapifydown = function (&$array, $k) use ($swap) {
+    $i = 0;
+    do {
+      $done = true;
+      $maxIdx = $i;
+      if(2*($i+1)-1 < $k) {
+        if($array[2*($i+1)-1] > $array[$maxIdx]) {
+          $maxIdx = 2*($i+1)-1;
+        }
+        if(2*($i+1) < $k) {
+          if($array[2*($i+1)] > $array[$maxIdx]) {
+            $maxIdx = 2*($i+1);
+          }
         }
       }
-    }
-    if($maxIdx !== $i) {
-      $done = false;
-      swap($array, $i, $maxIdx);
-      $i = $maxIdx;
-    }
-  } while (!$done);
-}
-
-function topksort(&$array, $k = 0) {
+      if($maxIdx !== $i) {
+        $done = false;
+        $swap($array, $i, $maxIdx);
+        $i = $maxIdx;
+      }
+    } while (!$done);
+  };
   if(0 === $k || count($array) < $k) {
     $k = count($array);
   }
   for($i = 1; $i < $k; ++$i) {
     $j = $i;
     while($j > 0 && $array[$j] > $array[intdiv($j, 2)]) {
-      swap($array, $j, intdiv($j, 2));
+      $swap($array, $j, intdiv($j, 2));
       $j = intdiv($j, 2);
     }
   }
   for($i = $k; $i < count($array); ++$i) {
     if($array[$i] < $array[0]) {
-      swap($array, $i, 0);
-      topkheapifydown($array, $k);
+      $swap($array, $i, 0);
+      $topkheapifydown($array, $k);
     }
   }
   for($i = $k-1; $i >= 0; --$i) {
-    swap($array, $i, 0);
-    topkheapifydown($array, $i);
+    $swap($array, $i, 0);
+    $topkheapifydown($array, $i);
   }
   while(count($array) > $k) {
     array_pop($array);
