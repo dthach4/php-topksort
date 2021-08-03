3ccf4adc4e8c130a22d39c0987eaacc98aa90c24
Dany Thach <d.thach4@gmail.com>
2021-08-03 02:19:36 +0200

    First commit

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+
+ini_set('memory_limit', '2G');
+
+include('topksort.php');
+
+$array = range(1, 1e7);
+
+shuffle($array);
+
+$array2 = $array;
+
+$start = microtime(true);
+topksort($array, 10000);
+$timeTaken = microtime(true) - $start;
+
+echo 'Time taken with topksort: '.$timeTaken.'s'.PHP_EOL;
+
+$start = microtime(true);
+sort($array2);
+$array2 = array_slice($array2, 0, 10000);
+$timeTaken = microtime(true) - $start;
+
+echo 'Time taken normally: '.$timeTaken.'s'.PHP_EOL;
